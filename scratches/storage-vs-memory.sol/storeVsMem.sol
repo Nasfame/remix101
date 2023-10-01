fc3968c458bf66119fdd725d33df98bcbde34e89
Nasfame <laciferin@gmail.com>
2023-10-01 12:56:57 +0530

    experiment complete between storage and memory

--- scratches/storage-vs-memory.sol/storeVsMem.sol
+++ scratches/storage-vs-memory.sol/storeVsMem.sol
@@ -5,45 +5,41 @@ contract helloGeeks
 {
   // Initialising array numbers
   int[] public numbers;
-  int [] public  memNumbers;
 
   constructor(){
     numbers = [1,2,3];
-    memNumbers = [1,2,3];
   }
  
   // Function to insert values
   // in the array numbers
   function NumbersWithStorage() public
   {
-    numbers.push(1);
-    numbers.push(2);
+    numbers.push(100);
+    numbers.push(200);
  
     //Creating a new instance
     int[] storage myArray = numbers;
+    // But actually a pointer
      
     // Adding value to the
     // first index of the new Instance
-    myArray[0] = 0;
+    myArray[0] = 1200;
   } 
-  function getAllNumbers() external  returns int[]{
+  function getAllNumbers() external view  returns(int[] memory){
         return numbers;
-  }
-
-  function getAllNumbersInMemory() external  returns int[]{
-        return memNumbers;
   }
 
    function NumbersWithMemory() public
   {
-    numbers.push(1);
-    numbers.push(2);
+    numbers.push(-1);
+    numbers.push(-2);
      
     //creating a new instance
     int[] memory myArray = numbers;
      
     // Adding value to the first
     // index of the array myArray
-    myArray[0] = 0;
+    myArray[0] = 1500;
+    // not reflected in numbers
   } 
 }
